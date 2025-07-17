9c33bb62f200da132f5a79b809539f785cc4b878
Andres Perea <andresfeliperea27@gmail.com>
2025-07-16 21:43:02 -0500

    navigation created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/pages/home/home';
+import { SignUp } from './components/pages/sign-up/sign-up';
+import { Multas } from './components/pages/multas/multas';
+import { Login } from './components/pages/login/login';
+import { PageNotFound } from './components/pages/page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", component: Home, title : "Home | SPA"},
+    {path: "sign-up", component: SignUp, title : "Sign Up| SPA"},
+    {path: "multas", component: Multas, title : "Multas | SPA"},
+    {path: "login", component: Login, title : "Login| SPA"},
+    {path: "", redirectTo: "/home", pathMatch: "full"},
+    {path: "**", component: PageNotFound, title : "Error 404 | SPA"}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navigation } from './components/shared/navigation/navigation';
+import { Footer } from './components/shared/footer/footer';
+import { Header } from './components/shared/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Navigation, RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'spa';
-}
+export class App {}
